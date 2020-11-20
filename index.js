eb337f823bdb3e91cb9fe456f64958c450d65514
biantao <biantao@360.cn>
2020-11-20 18:36:03 +0800

    add js

---
+++ index.js
@@ -0,0 +1,7 @@
+const body = document.querySelector('body')
+const viewHTMLList = document.querySelectorAll('.viewHTML')
+const viewCSSList = document.querySelectorAll('.viewCSS')
+
+body.addEventListener('click', (e) => {
+  const dom = e.target
+})

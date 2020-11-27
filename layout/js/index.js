e3f11df4c4317088e1a0001fc493a646bd400caa
shuaxin <986384495@qq.com>
2020-11-27 21:40:05 +0800

    更改代码结构

--- layout/js/index.js
+++ layout/js/index.js
@@ -1,8 +1,10 @@
-const body = document.querySelector('body')
-var HTMLList = document.querySelectorAll('.viewHTML')
-var CSSList = document.querySelectorAll('.viewCSS')
-var HTMLCodeList = document.querySelectorAll('.code-html')
-var CSSCodeList = document.querySelectorAll('.code-css')
+const Dom = {
+  body: document.querySelector('body'),
+  HTMLList: document.querySelectorAll('.viewHTML'),
+  CSSList: document.querySelectorAll('.viewCSS'),
+  HTMLCodeList: document.querySelectorAll('.code-html'),
+  CSSCodeList: document.querySelectorAll('.code-css'),
+}
 
 const findTargetDom = (domList, targetDom) => {
   let index = 0
@@ -15,18 +17,18 @@ const findTargetDom = (domList, targetDom) => {
   return index
 }
 
-body.addEventListener('click', (e) => {
+Dom.body.addEventListener('click', (e) => {
   const dom = e.target
   const targetType = dom.dataset.type
   // 根据type判断是都点击的是目标按钮
   if (targetType) {
     // 获取当前点击的dom索引
-    const targetIndex = findTargetDom(window[`${targetType}List`], dom)
+    const targetIndex = findTargetDom(Dom[`${targetType}List`], dom)
     let HTMLStatus = 'none', CSSStatus = 'block'
     if (targetType === 'HTML') CSSStatus = 'none', HTMLStatus = 'block'
     if (targetType === 'CSS') CSSStatus = 'block', HTMLStatus = 'none'
     // 设置当前操作的dom
-    HTMLCodeList[targetIndex].style.display = HTMLStatus
-    CSSCodeList[targetIndex].style.display = CSSStatus
+    Dom.HTMLCodeList[targetIndex].style.display = HTMLStatus
+    Dom.CSSCodeList[targetIndex].style.display = CSSStatus
   }
 })
